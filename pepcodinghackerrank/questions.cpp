710150e40f7da2c3f2aa6f1a269e0f7e93c7fed8
whoshivek <shivekagg25@gmail.com>
2020-06-17 18:06:24 +0530

    new

--- pepcodinghackerrank/questions.cpp
+++ pepcodinghackerrank/questions.cpp
@@ -4,57 +4,33 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-bool ques(vector <int> & arr , int idx)
+
+void permutation(string str , string myans)
 {
-    
-     if(idx == arr.size()-1)
-     {
-        return true;
-     }
+    if(str.length()==0)
+    {
+        cout<<myans<<endl;
+        return;
         
-     if(arr[idx]>=arr[idx+1]){
-       return false;}
+    }
+   
+    
+    
+         
+    for(int i =0 ; i< str.length() ; i++)
+    {
        
-
-       return ques(arr , idx +1);
-      
-          
-}
-
-////////////////////////////////////////
-int ques2(vector<int> & arr , int i , int data)
-{
-if (i == arr.size())
-{
-    return -1;
-}
-
-
-int smallans = ques2(arr , i +1 , data);
-if (smallans != -1)
-{
-    return smallans;
-}
-else
-{
-    return 77;
-}
-
-
-
-
-
+        permutation(str.substr(0 , i) + str.substr(i+1)   , myans + str.at(i) );
+    }
+        
+        
+ 
+    
 }
 
 int main() {
-    int n;
-    cin>>n;
-    vector<int> arr(n,0);
-    for(int i=0 ; i< n ;i++)
-    {
-        cin>>arr[i];
-    }
-    //cout<<ques(arr , 0);
-    cout<<ques2(arr,0 , 8);
+    string s;
+    cin>>s;
+    permutation(s , "");
     return 0;
 }
--- recursion/nqueen.cpp
+++ recursion/nqueen.cpp
@@ -1,38 +1,73 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void nqueen(vector<int> board , int qpsf,int tq , int idx , string path)
+
+bool IsSafeToPlaces(vector<vector<bool>> board , int x , int y)
 {
-static int c = 0;
-   if (qpsf == tq)
-   {
-       c++;
-       cout<<c<<"->"<<path<<endl;
-       return;
-   }
-   
-if (qpsf>tq)
+
+   vector <vector<int>> dir = { {0 , -1} , {-1 , -1} ,{ -1 ,0} , {-1 , 1}  };
+
+  for (int i = 0; i < dir.size(); i++)
+  {
+    for (int rad = 1; i < board.size(); rad++)
+    {
+      int  nx = x + rad * dir[i][0];
+        int ny = y + rad * dir[i][1];
+
+        if(nx < 0 || ny < 0 || nx>=board.size() || ny >= board[0].size())
+        {
+            break;
+        }
+        if(board[nx][ny])
+        {
+               return false;
+        }
+    
+    }
+    
+        
+  }
+
+        return true;
+}
+
+void nqueen(vector<vector<bool>> board , int idx , int qpsf , int tq , string path)
+{
+if (tq==qpsf)
+{
+    cout<<path<<endl;
+    return;
+}
+
+if(qpsf>tq)
 {
     return;
 }
 
-  
-   for (int i = idx ; i < board.size(); i++)
-   {
-       nqueen(board ,qpsf+1, tq , i + 1 ,path + "Q" + to_string(qpsf+1) + "Board" +to_string(i/4) + to_string(i%4) + " ");
-   }
-   
+for (int i = idx; i < board.size()*board[0].size(); i++)
+{  
+    int x = i/board.size();
+    int y = i%board.size();
+    if(IsSafeToPlaces(board , x , y) )
+    {
+           board [i/board.size()][i%board.size()]=true;
+     nqueen(board , i +1 , qpsf + 1, tq, path + "("+ to_string(i/board.size()) +"," + to_string(i%board.size()) + ")");
+           board [i/board.size()][i%board.size()]=false;
+    
+    }
+}
+
 
 
 }
 
+
 int main()
 {
 
-//         vector<vector<int>> arr(4 ) ;
+vector<vector<bool>> board( 4 , vector<bool>( 4 , false));
 
-    vector<int> board( 16);
+nqueen(board , 0 ,0 ,4 ,"");
 
-    nqueen(board , 0 , 4 , 0 , "");
 
 }
